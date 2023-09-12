7c402559d54fcb5684bc97e8babad87fe267ad6f
asm-shaikat <asmshaikat1952@gmail.com>
2023-09-12 13:15:29 +0600

    loops and vector

---
+++ Vector.m
@@ -0,0 +1,12 @@
+%Row Vector
+a = [10,20,30,50]
+%Column vector
+b = [10;20;30;50]
+%2d array
+mat = [1,2,3,4,5;6,7,8,9,10;11,12,13,14,15]
+%Showing specific element of a matrix
+mat(1,5)
+%Printing specific coulmn of a matrix
+x = mat(:,5)
+%Printing more than one column
+y = mat(:,2:5)
---
+++ loop.m
@@ -0,0 +1,13 @@
+a = 10;
+
+fprintf("Executing while loop")
+while(a<=15)
+    fprintf('%d\n',a);
+    a = a+1;
+end
+
+fprintf("Executing for loop")
+for a=10:15
+    fprintf("%d\n",a)
+end
+
